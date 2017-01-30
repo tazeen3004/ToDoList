e6afff7c47cb5b8eb2aa470a81d9fb96da6e8db8
tazeen khan <tazeenfirojkhan@gmail.com>
2017-01-30 19:33:10 +0530

    Displays list with delete option

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+     var list = ["Buy milk", "Run 5 miles", "Complete homework", "Plant a new plant"]
+    
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return(list.count)
+    }
+    
+     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+       let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+      cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCellEditingStyle.delete
+        {
+            self.list.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
